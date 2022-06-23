f006395ad2abc093ce50d7c102928931eb7712eb
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-23 10:36:37 -0700

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Template Repo',
       template: './src/index.html',
       inject: 'body'
     })
